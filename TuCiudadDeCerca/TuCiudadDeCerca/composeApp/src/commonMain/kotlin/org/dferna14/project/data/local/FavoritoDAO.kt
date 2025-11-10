290809cbed8a49ae30a11101f0f35c77d5298a62
daniiifdezz <dferna14@estudiantes.unileon.es>
2025-11-10 11:41:59 +0100

    Entidad favorito creada, DAO creado de favorito

--- TuCiudadDeCerca/TuCiudadDeCerca/composeApp/src/commonMain/kotlin/org/dferna14/project/data/local/FavoritoDAO.kt
+++ TuCiudadDeCerca/TuCiudadDeCerca/composeApp/src/commonMain/kotlin/org/dferna14/project/data/local/FavoritoDAO.kt
@@ -1,2 +1,24 @@
 package org.dferna14.project.data.local
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+//interacciones con la bbdd
+@Dao
+interface FavoritoDAO {
+
+    //replace para ids duplicados
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addFavorito(favorito: FavoritoEntidad)
+
+
+    @Query("DELETE FROM favoritos WHERE id = :id")
+    suspend fun removeFavorito(id: String)
+
+
+    @Query("SELECT * FROM favoritos")
+    fun getFavoritos(): Flow<List<FavoritoEntidad>>
+}
--- TuCiudadDeCerca/TuCiudadDeCerca/composeApp/src/commonMain/kotlin/org/dferna14/project/data/local/FavoritoEntidad.kt
+++ TuCiudadDeCerca/TuCiudadDeCerca/composeApp/src/commonMain/kotlin/org/dferna14/project/data/local/FavoritoEntidad.kt
@@ -1,2 +1,13 @@
 package org.dferna14.project.data.local
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//pintamos la tabla de favoritos
+@Entity(tableName = "favoritos")
+data class FavoritoEntidad(
+
+    @PrimaryKey
+    val id: String
+)
+
