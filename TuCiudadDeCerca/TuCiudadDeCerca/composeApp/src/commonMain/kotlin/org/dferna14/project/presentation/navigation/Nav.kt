e4e3cd64bf5408004f7072b44461556ec3691ba5
daniiifdezz <dferna14@estudiantes.unileon.es>
2025-11-17 10:55:11 +0100

    frontEnd mejorado, clase Wrapper compartida de fondo de Leon

--- TuCiudadDeCerca/TuCiudadDeCerca/composeApp/src/commonMain/kotlin/org/dferna14/project/presentation/navigation/Nav.kt
+++
@@ -1,6 +0,0 @@
-package org.dferna14.project.presentation.navigation
-
-import androidx.compose.runtime.Composable
-import org.dferna14.project.presentation.ListadoVM
-import org.dferna14.project.presentation.listado.ListadoScreen
-import org.dferna14.project.presentation.detalle.DetalleScreen
